8f9667e16689b9ef5b5226186f3a9696f8d0cf54
Cristopher <criscamacho1059@gmail.com>
2024-08-11 16:47:44 -0600

    Applicacion Nativa Esta cambio es el mayor para elv ersionamiento. 2.0.0

---
+++ app.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('Service Worker registrado con éxito:', registration);
+    }).catch(error => {
+      console.log('Error al registrar el Service Worker:', error);
+    });
+  }
+  
