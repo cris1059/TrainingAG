3581c2806bb82a06550059900c3bbc14d90f074d
cris1059 <149038713+cris1059@users.noreply.github.com>
2024-09-07 14:15:04 -0600

    Nuevo versionamiento del programa

--- app.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('Service Worker registrado con éxito:', registration);
-    }).catch(error => {
-      console.log('Error al registrar el Service Worker:', error);
-    });
-  }
-  
